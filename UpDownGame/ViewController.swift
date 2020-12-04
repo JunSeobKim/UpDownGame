83f1f35882a13bb02c37a53d0c7a191941ab233d
JunSeobKim <wnstjq616@gmail.com>
2020-12-05 01:38:19 +0900

    Connect ViewController IBOutlet and IBAction

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    // MARK: IBOutlets
+    @IBOutlet weak var resultUpDown: UILabel!
+    @IBOutlet weak var turnCount: UILabel!
+    @IBOutlet weak var inputValue: UITextField!
+    
+    // MARK: - Methods
+    @IBAction func touchUpSubmitButton(_ sender: UIButton) {
+    }
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
